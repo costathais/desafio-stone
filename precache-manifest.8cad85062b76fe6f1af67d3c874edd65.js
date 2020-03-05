af7b5deeafae146ed6c253e1ff99fd3aa82095c6
Thais Costa <tc.almeida15@gmail.com>
2020-03-05 12:24:17 -0300

    Updates

---
+++ precache-manifest.8cad85062b76fe6f1af67d3c874edd65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "616dea60162b8d412a7a1c1b4e08dd91",
+    "url": "/desafio-stone/index.html"
+  },
+  {
+    "revision": "c45f354fdc990270a2fb",
+    "url": "/desafio-stone/static/css/main.90b1c99f.chunk.css"
+  },
+  {
+    "revision": "9b9e00096c15a25c8e9e",
+    "url": "/desafio-stone/static/js/2.7624ec69.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/desafio-stone/static/js/2.7624ec69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c45f354fdc990270a2fb",
+    "url": "/desafio-stone/static/js/main.e6b603fa.chunk.js"
+  },
+  {
+    "revision": "295b63bfd262c16fd917",
+    "url": "/desafio-stone/static/js/runtime-main.a90edd3c.js"
+  }
+]);
